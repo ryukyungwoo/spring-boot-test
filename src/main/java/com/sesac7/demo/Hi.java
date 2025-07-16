747f1e6fd4470be67f930308fbb474a0bff77543
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-07-16 16:22:50 +0900

    hi6

--- src/main/java/com/sesac7/demo/Hi.java
+++ src/main/java/com/sesac7/demo/Hi.java
@@ -1,5 +1,7 @@
 package com.sesac7.demo;
 
+import com.sesac7.demo.entity.Greeting;
+import com.sesac7.demo.entity.GreetingRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class Hi {
+    private final GreetingRepository greetingRepository;
+
+    public Hi(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
     @GetMapping
     public String hi() {
+        greetingRepository.save(new Greeting("en", "Hello"));
+        greetingRepository.save(new Greeting("kr", "안녕하세요"));
+        greetingRepository.save(new Greeting("jp", "こんにちは"));
         return "hi";
     }
 }
