54566ecd9fd71556ec2aff08058df12ede12a457
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-07-17 15:07:58 +0900

    test 111

--- src/main/java/com/sesac7/demo/CorsConfig.java
+++ src/main/java/com/sesac7/demo/CorsConfig.java
@@ -11,7 +11,9 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")                         // 모든 엔드포인트에 대해
                 .allowedOrigins("http://localhost:3000",
-                        "http://192.168.20.235:3000"
+                        "http://192.168.20.235:3000",
+                        "http://localhost",
+                        "http://192.168.20.235"
                         )   // 허용할 Origin (Next.js dev 서버)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")                       // 모든 헤더 허용
